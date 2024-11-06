2163de4528d1341d2cb4700ed9d298cb48d45042
juanpablotr14 <infobethlemitastascon@gmail.com>
2024-11-06 16:24:07 -0500

    Login User

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LoginComponent } from "./user/auth/login/login.component";
+import { UserLoginComponent } from './user/auth/user-login/user-login.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginComponent],
+  imports: [RouterOutlet, UserLoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
