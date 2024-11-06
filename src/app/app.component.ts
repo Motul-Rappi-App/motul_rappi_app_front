9b4dacde39988b1dc33941a1d54d6766423510e0
Jurgen212 <jurgensanclemente@gmail.com>
2024-11-06 17:03:58 -0500

    Previous

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PrincipalLayoutComponent } from './layouts/principal-layout/principal-layout.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PrincipalLayoutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
