05913ac938b0b059c2a2cd7a905ce1490162c96a
juanpablotr14 <infobethlemitastascon@gmail.com>
2024-11-07 18:52:52 -0500

    Redeem Discount

--- src/app/user/UI/redeem-discount/redeem-discount.component.ts
+++ src/app/user/UI/redeem-discount/redeem-discount.component.ts
@@ -1,12 +1,44 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormsContainerComponent } from '../../../layouts/forms-container/forms-container.component';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-redeem-discount',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule, FormsContainerComponent],
   templateUrl: './redeem-discount.component.html',
   styleUrl: './redeem-discount.component.css'
 })
 export class RedeemDiscountComponent {
+
+  redeemForm: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private _toast: ToastrService
+  ) {
+    this.redeemForm = this.fb.group({
+      cedula: ['', [Validators.required, Validators.pattern(/^\d+$/), Validators.minLength(8)]],
+    });
+  }
+
+  onSubmit() {
+    if (this.redeemForm.valid ) {
+      console.log('Formulario válido:', this.redeemForm.value);
+      this._toast.success('Verificando', 'Cedula Recibida', {
+        timeOut: 3000,
+        progressBar: true,
+      });
+    } else {
+      this._toast.error('Formulario inválido', 'Error al verificar', {
+        timeOut: 3000,
+        progressBar: true,
+      });
+    }
+  }
+
+  get cedula() { return this.redeemForm.get('cedula'); }
 
 }
