c18d839f2e7899ea931ced8f7fc78e7c4adeee9d
juanpablotr14 <infobethlemitastascon@gmail.com>
2024-11-09 14:34:52 -0500

    Spinner and Redirectons update

--- src/app/shared/spinner/spinner.component.ts
+++ src/app/shared/spinner/spinner.component.ts
@@ -1,7 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-spinner',
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './spinner.component.html',
   styleUrls: ['./spinner.component.css']
 })
